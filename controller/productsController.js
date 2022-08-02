d775d2bd3ce2a95162f573ee09f62450eeda40ed
Dagim Cherinet <80765887+dagim-cherinet@users.noreply.github.com>
2022-08-02 20:22:18 +0300

    added parsin request body mechanism

--- controller/productsController.js
+++ controller/productsController.js
@@ -11,10 +11,19 @@ async function getProduct(req, res, id) {
   res.end(JSON.stringify(product));
 }
 async function createProduct(req, res) {
+  //static data just for testing purpose
   const data = { name: "test", desc: "this is test product" };
-  const newProduct = await Product.createProduct(data);
-  res.writeHead(201, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(newProduct));
+  //the actual data from the request body
+  let bodyData = "";
+
+  req.on("data", (chunk) => (bodyData += chunk));
+  req.on("end", async () => {
+    // product = JSON.parse(bodyData);
+    // console.log(product);
+    const newProduct = await Product.createProduct(JSON.parse(bodyData));
+    res.writeHead(201, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(newProduct));
+  });
 }
 async function notFound(req, res) {
   res.writeHead(404, { "Content-Type": "application/json" });
